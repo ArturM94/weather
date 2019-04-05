9b5f731ce7ddad73b021d3f9272e6b2d193d2f00
Artur Manukian <manukian.artur.94@gmail.com>
2019-04-05 14:43:36 +0300

    Add component state in App

--- src/App.js
+++ src/App.js
@@ -8,19 +8,41 @@ const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
 
 class App extends Component {
 
+  state = {
+    temp: undefined,
+    city: undefined,
+    country: undefined,
+    sunrise: undefined,
+    sunset: undefined,
+  };
+
   getCurrentWeather = async (event) => {
     event.preventDefault();
     const city = event.target.elements.city.value;
     const apiUrl = await fetch(`${baseUrl}${city}&appid=${apiKey}&units=metric`);
     const data = await apiUrl.json();
     console.log(data);
+
+    this.setState({
+      temp: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      sunrise: data.sys.sunrise,
+      sunset: data.sys.sunset,
+    });
   };
 
   render() {
     return (
       <div className="App">
         <Form getCurrentWeather={this.getCurrentWeather} />
-        <Weather />
+        <Weather
+          temp={this.state.temp}
+          city={this.state.city}
+          country={this.state.country}
+          sunrise={this.state.sunrise}
+          sunset={this.state.sunset}
+        />
       </div>
     );
   }
