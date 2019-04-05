bba0e89a0a2b2f814f2fa1a8e53c08a3fd60f476
Artur Manukian <manukian.artur.94@gmail.com>
2019-04-05 14:00:46 +0300

    Add getCurrentWeather() method

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ const apiKey = 'f38816136abbc40b2492acabf0d36e10';
 const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
 
 class App extends Component {
+
+  getCurrentWeather = async (event) => {
+    event.preventDefault();
+    const city = event.target.elements.city.value;
+    const apiUrl = await fetch(`${baseUrl}${city}&appid=${apiKey}&units=metric`);
+    const data = await apiUrl.json();
+    console.log(data);
+  };
+
   render() {
     return (
       <div className="App">
