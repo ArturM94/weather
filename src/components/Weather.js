a1b2d5973dab78c5bc5bd8f3ced293753e5dc40e
Artur Manukian <manukian.artur.94@gmail.com>
2019-04-09 17:59:54 +0300

    Add className for tags in App component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
 const Weather = (props) => (
-  <div>
-    {props.city && props.country && <p>Temperature in <strong>{props.city}, {props.country}</strong></p>}
-    {props.temp && <h1>{props.temp}&deg;C</h1>}
-    {props.humidity && <p>Humidity: {props.humidity}&#37;</p>}
-    {props.description && <p>Condition: {props.description}</p>}
-    {props.error && <p>{props.error}</p>}
+  <div className='weather__info'>
+    {
+      props.city && props.country && <p className='weather__key'>Location:
+        <span className='weather__value'> {props.city}, {props.country}</span>
+      </p>
+    }
+    {
+      props.temp && <p className='weather__key'>Temperature:
+        <span className='weather__value'> {props.temp}&deg;C</span>
+      </p>
+    }
+    {
+      props.humidity && <p className='weather__key'>Humidity:
+        <span className='weather__value'> {props.humidity}&#37;</span>
+      </p>
+    }
+    {
+      props.description && <p className='weather__key'>Condition:
+        <span className='weather__value'> {props.description}</span>
+      </p>
+    }
+    {props.error && <p className='weather__error'>
+      <span> {props.error}</span>
+    </p>}
   </div>
 );
 
