7bd4e6bb0476475a970501a3992969d7fcdea6ee
Artur Manukian <manukian.artur.94@gmail.com>
2019-04-09 17:58:15 +0300

    Add bootstrap layers and columns in App component

--- src/App.js
+++ src/App.js
@@ -47,12 +47,24 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Title/>
-        <Form getWeather={this.getWeather}/>
-        <Weather
-          {...this.state}
-        />
+      <div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-5 title-container">
+                  <Title/>
+                </div>
+                <div className="col-7 form-container">
+                  <Form getWeather={this.getWeather}/>
+                  <Weather
+                    {...this.state}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
